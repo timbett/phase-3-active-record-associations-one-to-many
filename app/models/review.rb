e7d2e12211b7d05e8529e7eb9e163f9b6fe4f1f2
Timothy <tkkibet13@gmail.com>
2022-11-06 10:24:30 +0300

    Assigniment completed

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
-  
+    belongs_to :game
+    # a review belongs to a game
+   # def game
+        # self is the review instance
+    #    Game.find(self.game_id)
+    #end
 end
